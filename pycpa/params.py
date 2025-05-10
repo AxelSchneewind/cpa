b2a9cd9d2c1d95921aff48a457b26bd92e69315e
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2025-05-11 01:00:01 +0800

    accept multiple programs for verification

--- pycpa/params.py
+++ pycpa/params.py
@@ -2,10 +2,12 @@ import argparse
 
 
 parser = argparse.ArgumentParser(prog='pycpa', usage='TODO', description='python implementation of CPA')
-parser.add_argument('-p', '--program', help='the program to validate')
+parser.add_argument('program', help='the program to validate', nargs='+')
 
 parser.add_argument('-c', '--config', help='which analysis configuration to use (use --list-configs to get a list of available ones)')
-parser.add_argument('--list-configs', help='a list of available ones analyses')
+parser.add_argument('-p', '--property', help='which analysis configuration to use (use --list-configs to get a list of available ones)')
+parser.add_argument('--list-configs', help='a list of available analyses')
+parser.add_argument('--list-properties', help='a list of available properties')
 
 # TODO add parameters
 
