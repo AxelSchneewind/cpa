42c00a493cceb843e480490c9b7cec39bafdc21d
Axel Schneewind <axel.schneewind@protonmail.com>
2025-05-11 14:36:27 +0800

    rename variable

--- benchmarks/transpile_set.sh
+++ benchmarks/transpile_set.sh
@@ -4,11 +4,11 @@ VENV_DIR=../venv/
 
 source ./prepare_c.sh
 
-set="$(basename --suffix=.set $1)"
-echo "Transforming set $set"
+benchset="$(basename --suffix=.set $1)"
+echo "Transforming set $benchset"
 
-file=$(cat $set.set)
-mkdir -p "tmp_$set" "$set"
+file=$(cat $benchset.set)
+mkdir -p "tmp_$benchset" "$benchset"
 
 # get files
 for line in $file
@@ -18,25 +18,28 @@ do
     # filter out files with gotos: not supported yet
     [ -n "$(grep 'goto' $line)" ] && echo "skipping $line" && continue
 
-    cp "$line" "tmp_$set/$c_file"
+    ln -s "../$line" $benchset/
+
+    cp "$line" "tmp_$benchset/$c_file"
 done
 
 # transform files
-tmp_files=$(echo tmp_$set/*)
+tmp_files=$(echo tmp_$benchset/*)
 for file in $tmp_files
 do
     c_file=$(basename $file)
 
-
     # prepare
-    prepare_c "tmp_$set/$c_file"
+    prepare_c "tmp_$benchset/$c_file"
 
-    echo "transpiling $line"
+    echo "transpiling $c_file"
 
     # transform
-    $VENV_DIR/bin/python c2py "tmp_$set/$c_file" "$set/$c_file.py" >/dev/null 2>/dev/null
-    [ ! "$?"=="0" ] && rm tmp_$set/$c_file
+    $VENV_DIR/bin/python c2py "tmp_$benchset/$c_file" "$benchset/$c_file.py" >/dev/null 2>/dev/null
+    [ ! "$?"=="0" ] && rm tmp_$benchset/$c_file
+
+
 done
 
-rm -rf "tmp_$set"
+rm -rf "tmp_$benchset"
 
