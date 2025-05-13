bc3a57bc9cfa9c9fc545e13d126eab0357f2fcb0
Axel Schneewind <axel.schneewind@protonmail.com>
2025-05-13 13:21:09 +0800

    add check for benchmark set existence

--- benchmarks/transpile_set.sh
+++ benchmarks/transpile_set.sh
@@ -7,10 +7,18 @@ source ./prepare_c.sh
 benchset="$(basename --suffix=.set $1)"
 echo "Transforming set $benchset"
 
-file=$(cat $benchset.set)
-mkdir -p "tmp_$benchset" "$benchset"
+
+[ ! -e $benchset.set ] && echo "could not find benchset $1" && exit
+
+
+tmp_dir="tmp_$benchset"
+mkdir -p "$tmp_dir" 
+mkdir -p "$benchset"
 
 # get files
+echo "Copying files from $benchset"
+
+file=$(cat $benchset.set)
 for line in $file
 do
     c_file="$(basename $line)"
@@ -18,28 +26,28 @@ do
     # filter out files with gotos/switch: not supported yet
     ([ -n "$(grep 'goto' $line)" ] || [ -n "$(grep 'switch' $line)" ]) && echo "skipping $line" && continue
 
-    ln -s "../$line" $benchset/
+    ln -sf "../$line" $benchset/
 
-    cp "$line" "tmp_$benchset/$c_file"
+    cp "$line" "$tmp_dir/$c_file"
 done
 
+
 # transform files
-tmp_files=$(echo tmp_$benchset/*)
+echo "Transpiling files from $benchset"
+tmp_files=$(echo $tmp_dir/*)
 for file in $tmp_files
 do
     c_file=$(basename $file)
 
     # prepare
-    prepare_c "tmp_$benchset/$c_file"
+    prepare_c "$tmp_dir/$c_file"
 
     echo "transpiling $c_file"
 
     # transform
-    $VENV_DIR/bin/python c2py "tmp_$benchset/$c_file" "$benchset/$c_file.py" >/dev/null 2>$benchset/error_$c_file
-    [ ! "$?"=="0" ] && rm tmp_$benchset/$c_file
-
-
+    $VENV_DIR/bin/python c2py "${tmp_dir}/${c_file}" "$benchset/${c_file}.py" 2>$benchset/error_${c_file}
+    ([ -n "$(cat $benchset/error_${c_file})" ] && echo "error for ${c_file}" && rm -f $benchset/${c_file}.py) || rm $benchset/error_${c_file}
 done
 
-rm -rf "tmp_$benchset"
+rm -rf "$tmp_dir"
 
