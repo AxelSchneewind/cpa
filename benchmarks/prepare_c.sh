20cb9bf20204116c2367f690648fcf99c9094d53
Axel Schneewind <axel.schneewind@protonmail.com>
2025-05-13 13:20:26 +0800

    fix preprocessing

--- benchmarks/prepare_c.sh
+++ benchmarks/prepare_c.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
 prepare_c() {
-local file="$1"
-# replace ERROR label with reach_error call
-sed -i -e 's/^.*ERROR:.*$/\treach_error();/' "$file"
-# remove abort declaration
-sed -i -e 's/^.*extern\s*void\s*abort.*;/void abort(void) { }/' "$file"
+    local file="$1"
 
-# remove __assert_fail declaration
-sed -i -e 's/^.*extern\s*void\s*__assert_fail.*;//' "$file"
+    # remove __assert_fail declaration
+    sed -i -e 's/^.*extern\s*void\s*__assert_fail.*;//' "$file"
 
-# replace reach_error implementation
-sed -i -e 's/^\s*void\s*reach_error().*$/void reach_error() {}/' "$file"
+    # replace abort declaration
+    sed -i -e 's/^.*extern\s*void\s*abort\s*{[^}]*}/void abort(void) { }/' "$file"
+
+    # replace reach_error implementation
+    sed -i -e 's/^\s*void\s*reach_error()\s{[^}]*}/void reach_error() {}/' "$file"
+
+    # replace ERROR label and code block with reach_error call
+    sed -i -e 's/\s*ERROR:\s*{[^}]*}/reach_error();/' "$file"
+
+    # replace ERROR label and statement with reach_error call
+    sed -i -e 's/\s*ERROR:\s*.*;/reach_error();/' "$file"
+
+    # replace ERROR label with reach_error call
+    sed -i -e 's/\s*ERROR:\s*/reach_error();/' "$file"
+    
+    # remove includes
+    sed -i -e '/^\s*#\s*include.*$/d' "$file"
 }
 
