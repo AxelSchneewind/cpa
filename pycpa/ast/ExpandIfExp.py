e78cb4cd3568644b7ae81e98b419be868886b0bb
Axel Schneewind <axel.schneewind@protonmail.com>
2025-05-22 11:56:27 +0800

    expand ternary expressions and check intermediate asts

---
+++ pycpa/ast/ExpandIfExp.py
@@ -0,0 +1,56 @@
+import ast
+from pycpa.ast.StatementExtractor import StatementExtractor
+
+class ExpandIfExp(StatementExtractor):
+    """
+        AST transformer that transforms IfExps (ternary expressions)
+        into statements
+    """
+
+    def __init__(self):
+        self.instruction_stack = list()
+        self.current_tmp_ctr = 0
+
+        # stores whether a visited call has to be extracted
+        # always the case if not a single expression
+        self.extract = False
+        self.extract_expr = False
+
+    def assign_result_to(self, call : ast.AST, return_var : str) -> ast.AST:
+        expr = ast.Assign(
+            targets = [ast.Name(return_var, ctx=ast.Store())],
+            value = call
+        )
+        ast.copy_location(call, expr)
+        ast.fix_missing_locations(expr)
+        return expr
+
+
+    def visit_IfExp(self, node: ast.IfExp) -> ast.Name:
+        # if not already handled by assign: use temporary variable
+        name = self.fresh_tmp_var()
+        branch = ast.If(
+            test=node.test,
+            body=[ast.Assign([ast.Name(name, ctx=ast.Store())], self.visit(node.body))],
+            orelse=[ast.Assign([ast.Name(name, ctx=ast.Store())], self.visit(node.orelse))]
+        )
+        ast.copy_location(branch, node)
+        ast.fix_missing_locations(branch)
+
+        return self.extract_expression(branch)
+
+    def visit_Assign(self, node: ast.Assign) -> ast.Name:
+        if not isinstance(node.value, ast.IfExp):
+            return node
+
+        branch = ast.If(
+            test=node.value.test,
+            body=[ast.Assign(node.targets, self.visit(node.value.body))],
+            orelse=[ast.Assign(node.targets, self.visit(node.value.orelse))]
+        )
+        ast.copy_location(branch, node)
+        ast.fix_missing_locations(branch)
+
+        self.push_instruction(branch)
+        return None
+            
--- pycpa/preprocessor.py
+++ pycpa/preprocessor.py
@@ -1,12 +1,15 @@
-from pycpa.ast import ExpandAugAssign, ASTPreprocessor, EnsureReturn, RemoveBuiltins, ASTVisualizer, SetExecutionContext
+from pycpa.ast import (
+    ExpandAugAssign, ASTPreprocessor, EnsureReturn, RemoveBuiltins, 
+    ASTVisualizer, SetExecutionContext, ExpandIfExp
+)
 from pycpa.cfa import builtin_identifiers
 
 import ast
 
 transformers = [
     ExpandAugAssign(),
+    ExpandIfExp(),
     RemoveBuiltins(set(builtin_identifiers.keys())),
-    SetExecutionContext(),
     EnsureReturn(),
     ASTPreprocessor(),
 ]
@@ -14,5 +17,6 @@ transformers = [
 def preprocess_ast(tree : ast.AST) -> ast.AST:
     for t in transformers:
         tree = t.visit(tree)
+        ast.unparse(tree)       # check if ast is valid, i.e. can be unparsed
     return tree
 
