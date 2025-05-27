3f51d38a782db5a95a8a292fcb139cb3259ae222
Axel Schneewind <axel.schneewind@protonmail.com>
2025-05-27 18:45:41 +0800

    add yaml dependency

--- pycpa-tooldef.py
+++ pycpa-tooldef.py
@@ -10,16 +10,20 @@ import benchexec.util as util
 import benchexec.tools.template
 import benchexec.model
 
+from benchexec.tools.sv_benchmarks_util import (
+    get_non_witness_input_files_or_identifier,
+)
 
-class Tool(benchexec.tools.template.BaseTool):
+# class Tool(benchexec.tools.template.BaseTool):
+class Tool(benchexec.tools.template.BaseTool2):
     """
     Tool info for pycpa.
     """
 
-    REQUIRED_PATHS = ["bin", "helper", "scripts"]
+    REQUIRED_PATHS = ["scripts"]
 
-    def executable(self):
-        return util.find_executable("scripts/cpa.sh")
+    def executable(self, tool_locator):
+        return tool_locator.find_executable("cpa.sh", subdir="scripts")
 
     def program_files(self, executable):
         return self._program_files_from_executable(
@@ -27,8 +31,7 @@ class Tool(benchexec.tools.template.BaseTool):
         )
 
     def version(self, executable):
-        version = self._version_from_tool(executable, line_prefix="pycpa")
-        return version.split("Version ")[1]
+        return self._version_from_tool(executable, line_prefix='pycpa ')
 
     def name(self):
         return "pycpa"
@@ -36,7 +39,21 @@ class Tool(benchexec.tools.template.BaseTool):
     def project_url(self):
         return "https://https://github.com/AxelSchneewind/pycpa"
 
-    def determine_result(self, returncode, returnsignal, output, isTimeout):
+
+    def _get_additional_options(self, existing_options, task, rlimits):
+        options = []
+
+        if task.property_file:
+            options += ["--property", task.property_file]
+
+        return options
+
+    def cmdline(self, executable, options, task, rlimits):
+        additional_options = self._get_additional_options(options, task, rlimits)
+        input_files = get_non_witness_input_files_or_identifier(task)
+        return [executable] + options + additional_options + input_files + ['--compact']
+
+    def determine_result(self, run):
         """
         Parse the output of the tool and extract the verification result.
         This method always needs to be overridden.
@@ -46,14 +63,14 @@ class Tool(benchexec.tools.template.BaseTool):
         and should give some indication of the failure reason
         (e.g., "CRASH", "OUT_OF_MEMORY", etc.).
         """
-        for line in output:
+        for line in run.output:
             if "All test cases time out or crash, giving up!" in line:
                 return "Couldn't run: all seeds time out or crash"
             if "ERROR:" in line:
                 return "Couldn't run pycpa"
             if "FALSE" in line:
-                return result.RESULT_FALSE_REACH
+                return result.RESULT_FALSE_PROP
             if "TRUE" in line:
-                return result.RESULT_DONE
+                return result.RESULT_TRUE_PROP
             if "UNKNOWN" in line:
                 return result.RESULT_UNKNOWN
