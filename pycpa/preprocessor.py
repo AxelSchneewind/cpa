ee0583b19e865db4b22fc22645c2730b8b175cef
Axel Schneewind <axel.schneewind@protonmail.com>
2025-05-31 16:31:05 +0800

    remove commented out code

--- pycpa/preprocessor.py
+++ pycpa/preprocessor.py
@@ -17,19 +17,7 @@ transformers = [
     ASTPreprocessor(),
 ]
 
-# def preprocess_ast(tree : ast.AST) -> ast.AST:
-#     for t in transformers:
-#         tree = t.visit(tree)
-#         tree = ast.fix_missing_locations(tree)
-#         print("Tree: ", tree)
-#         try:
-#             ast.unparse(tree)
-#         except Exception as e:
-#             print("Unparse failed after", t.__class__.__name__)
-#             raise e       # check if ast is valid, i.e. can be unparsed
-#     return tree 
-
-def preprocess_ast(tree: ast.AST) -> ast.AST:
+def preprocess_ast(tree : ast.AST) -> ast.AST:
     for t in transformers:
         tree = t.visit(tree)
         tree = ast.fix_missing_locations(tree)
