726cf6a7c5c4e720fc7ca897135dd75159eae3e2
Axel Schneewind <axel.schneewind@protonmail.com>
2025-06-02 01:31:21 +0800

    fix transpilation and also include task yml files

--- benchmarks/transpile_set.sh
+++ benchmarks/transpile_set.sh
@@ -21,33 +21,39 @@ echo "Copying files from $benchset"
 file=$(cat $benchset.set)
 for line in $file
 do
-    c_file="$(basename $line)"
+    y_file="$line"
+    y_path=$(dirname $y_file)
+
+    c_file=$(grep "input_files: '.*'" $y_file | sed "s/input_files: '//" | sed "s/'//")
+    c_file=${y_path}/$c_file
+    echo $c_file
 
     # filter out files with gotos/switch: not supported yet
-    ([ -n "$(grep 'goto' $line)" ]) && echo "skipping $line" && continue
+    ([ -n "$(grep -f blacklist-keywords.txt $c_file)" ]) && echo "skipping $c_file" && continue
 
-    ln -sf "../$line" $benchset/
-
-    cp "$line" "$tmp_dir/$c_file"
+    cp "$y_file" $benchset/
+    cp "$c_file" $benchset/
 done
 
 
 # transform files
 echo "Transpiling files from $benchset"
-tmp_files=$(echo $tmp_dir/*)
-for file in $tmp_files
+c_files=$(echo $benchset/*.c)
+for c_file in $c_files
 do
-    c_file=$(basename $file)
-
     # prepare
-    prepare_c "$tmp_dir/$c_file"
+    prepare_c "$c_file"
+    fname=$(basename $c_file)
 
     echo "transpiling $c_file"
 
     # transform
-    $VENV_DIR/bin/python c2py "${tmp_dir}/${c_file}" "$benchset/${c_file}.py" 2>$benchset/error_${c_file}
-    ([ -n "$(cat $benchset/error_${c_file})" ] && echo "error for ${c_file}" && rm -f $benchset/${c_file}.py) || rm $benchset/error_${c_file}
-done
+    $VENV_DIR/bin/python c2py "${c_file}" "${c_file}.py" 2> $benchset/error_${fname}
 
-rm -rf "$tmp_dir"
+    [ -n "$(cat $benchset/error_${fname})" ] && echo "error for ${fname}"
+    [ -n "$(cat $benchset/error_${fname})" ] && rm -f $benchset/${fname}.py
+    [ -n "$(cat $benchset/error_${fname})" ] && rm -f $benchset/${basename}.yml
+    [ -n "$(cat $benchset/error_${fname})" ] || rm $benchset/error_${fname}
+
+done
 
