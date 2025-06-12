ae53e9d8c17fb761ffc0d68161c24cf76ac5b563
Axel Schneewind <axel.schneewind@protonmail.com>
2025-06-12 19:01:02 +0200

    add symbols to ignore when transpiling

--- benchmarks/transpile.sh
+++ benchmarks/transpile.sh
@@ -6,7 +6,7 @@ cpref=$(dirname $cfile)
 
 [ -e $cfile ] || (echo "source file $cfile missing" && exit 1)
 
-python ../c2py "${cfile}" "${cname}.py"  2> "error_${cname}"
+python ../c2py "${cfile}" "${cname}.py" "$2"  2> "error_${cname}"
 
 # remove empty error file
 [ -n "$(cat error_${cname})" ] || rm error_${cname}
