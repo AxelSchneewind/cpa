a4d19ecdb17bb9c2e704b5a7aeccc056e0337462
Axel Schneewind <axel.schneewind@protonmail.com>
2025-06-12 13:00:11 +0200

    add test program for branches of different lengths

---
+++ test_progs/absolutely_trivial_branch_merging_unsafe.py
@@ -0,0 +1,28 @@
+def reach_error():
+    ''' abort with error message in actual execution, is ignored by verifier '''
+    print('reached error')
+    exit(1)
+
+def VERIFIER_assert(cond):
+    if not cond:
+        reach_error()
+
+def main():
+    ''' program entry point '''
+    x = __VERIFIER_nondet_int()
+    y = 0
+
+    if x == 1:
+        y = 2       # extra assignment to increase ssa index
+        y = 1
+    else:
+        y = 1
+
+    # always false
+    VERIFIER_assert(y != 1)
+
+
+
+if __name__ == '__main__':
+    ''' entry point for actual execution '''
+    main()
