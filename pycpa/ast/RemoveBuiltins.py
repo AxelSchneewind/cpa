5dee9371429b3d3fba163af214575de3194b538c
Axel Schneewind <axel.schneewind@protonmail.com>
2025-06-17 00:31:01 +0200

    fix RemoveBuiltins

--- pycpa/ast/RemoveBuiltins.py
+++ pycpa/ast/RemoveBuiltins.py
@@ -13,7 +13,7 @@ class RemoveBuiltins(ast.NodeTransformer):
     def __init__(self, builtin_identifiers):
         self.builtin = builtin_identifiers
         self.pattern = re.compile(r'^__(tmp|ret)')
-    
+
     def visit_Call(self, node) -> ast.Call:
         """
         removes call to int() as all variables are assumed to be integers
@@ -21,7 +21,7 @@ class RemoveBuiltins(ast.NodeTransformer):
         assert isinstance(node.func, ast.Name)
         name = node.func.id
         match name:
-            case 'int':
+            case 'int' | 'float' | 'bool':
                 return node.args[0]
             case _:
                 return node
@@ -33,22 +33,11 @@ class RemoveBuiltins(ast.NodeTransformer):
         if node.name in self.builtin:
             return None
         else:
-            return node
+            return ast.NodeTransformer.generic_visit(self, node)
 
-    def visit_Name(self, node) -> ast.Name:
-        """
-            rename variables that use __ prefix (marks verifier-internal identifiers)
-        """
-        if self.pattern.match(node.id):
-            newname = ast.Name(
-                id=str('USER') + node.id,
-                ctx=node.ctx
-            )
-            ast.copy_location(newname, node)
-            return newname
-        else:
-            return node
-            
+    def visit_AnnAssign(self, node) -> ast.AnnAssign:
+        node.target = self.visit(node.target)
+        return node
 
 
 
