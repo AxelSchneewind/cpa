6318da34abd490306f6e35fd6a9d39fd827259c3
Axel Schneewind <axel.schneewind@protonmail.com>
2025-06-17 12:10:10 +0200

    add more test programs

---
+++ test_progs/always_even_large_unsafe.py
@@ -0,0 +1,26 @@
+def reach_error():
+    ''' abort with error message in actual execution, is ignored by verifier '''
+    print('reached error')
+    exit(1)
+
+def VERIFIER_assert(cond):
+    if not cond:
+        reach_error()
+
+def main():
+    ''' program entry point '''
+    n = __VERIFIER_nondet_int()
+    n = n % (2**32)
+
+    x = 0
+    i = 0
+    while i < n:
+        x += 2
+        i += 1
+
+    VERIFIER_assert((x % 2) == 1)
+
+
+if __name__ == '__main__':
+    ''' entry point for actual execution '''
+    main()
